056d760e670e9c1f0139b53e5d3c5e6249c73086
Alexey Khrusch <alexeykhr@outlook.com>
2018-11-21 21:13:27 +0200

    webpack - add support Vue

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+import App from './App.vue'
+import Vue from 'vue'
+
+// Import styles
 import './style.css'
+
+// Prevent the production tip on Vue startup
+Vue.config.productionTip = false
+
+const app = new Vue({
+	el: '#app',
+	render: h => h(App)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,50 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const packageJSON = require('./package')
+const { VueLoaderPlugin } = require('vue-loader')
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
-  entry: packageJSON.main,
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html',
-      inject: true,
-      chunksSortMode: 'dependency'
-    })
-  ]
+	mode: 'development',
+	entry: [
+		'./src/main.js'
+	],
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.vue$/,
+				use: 'vue-loader'
+			}
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html',
+			inject: true,
+			chunksSortMode: 'dependency'
+		})
+	]
 }
