1d59a42b9d65cdcf623a6d36fc242807c487c3aa
Alexey Khruhsch <alexeykhr@outlook.com>
2019-03-25 20:52:55 +0200

    Add more tips

--- src/data/tips.js
+++ src/data/tips.js
@@ -1,5 +1,10 @@
 'use strict'
 
+import { list as roleList} from '@/data/roles'
+import * as roles from '@/enum/roles'
+
+const adminUser = `<strong class="${roleList[roles.ADMIN].color}">${roleList[roles.ADMIN].name}</strong>`
+
 /**
  * @var {array}
  * @example
@@ -7,7 +12,25 @@
  *  - access (array of enum roles)
  */
 export default [
-	{ text: '<strong>Esc</strong> - закрити глобальний пошук' },
-	{ text: '<strong>Shift + Mouse Wheel</strong> - переміщати горизонтальний скролл' },
-	{ text: '<strong>Ctrl + Shift + F</strong> - відкрити глобальний пошук' }
+	{
+		text: '<strong>Esc</strong> - закрити глобальний пошук.'
+	},
+	{
+		text: '<strong>Shift + Mouse Wheel</strong> - переміщати горизонтальний скролл.'
+	},
+	{
+		text: '<strong>Ctrl + Shift + F</strong> - відкрити глобальний пошук.'
+	},
+	{
+		text: `Тільки ${adminUser} має можливість бачити ролі інших користувачів.`,
+		access: [roles.ADMIN]
+	},
+	{
+		text: `${adminUser} не може змінювати пароль користувача вручну, тільки сгенерувати.`,
+		access: [roles.ADMIN, roles.WORKER]
+	},
+	{
+		text: 'Всі фотографії на сайті доступні тільки для зареєстрованих користувачів.',
+		access: [roles.ADMIN, roles.WORKER]
+	}
 ]
