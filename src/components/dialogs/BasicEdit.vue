a76c879ee0e6f24059c5dc59b022efd4fc0a51ac
Alexey Khruhsch <alexeykhr@outlook.com>
2019-04-01 19:02:45 +0300

    Add BasicEdit and replace users Edit dialog

---
+++ src/components/dialogs/BasicEdit.vue
@@ -0,0 +1,41 @@
+<template>
+	<el-dialog
+		class="dialog--default"
+		v-bind="$attrs"
+		v-on="$listeners"
+	>
+		<div class="content">
+			<slot />
+		</div>
+		<span slot="footer">
+			<el-button @click="onClose">Закрити</el-button>
+			<el-button
+				type="primary"
+				:loading="loading"
+				:disabled="loading"
+				@click="onSubmit"
+			>
+				Зберегти
+			</el-button>
+		</span>
+	</el-dialog>
+</template>
+
+<script>
+export default {
+	inheritAttrs: false,
+	computed: {
+		loading() {
+			return this.$attrs.loading
+		}
+	},
+	methods: {
+		onSubmit() {
+			this.$emit('submit')
+		},
+		onClose() {
+			this.$emit('close')
+		}
+	}
+}
+</script>
