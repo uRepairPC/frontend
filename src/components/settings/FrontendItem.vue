4c6512be3476b8afa86694fe0b8de42b75403f67
Alexey Khruhsch <alexeykhr@outlook.com>
2019-04-04 20:33:13 +0300

    Display settings to Settings/Global page. Delete image

---
+++ src/components/settings/FrontendItem.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<template v-if="type === 'img'">
+			<img
+				v-if="value"
+				:src="server + value"
+				:alt="attr"
+			>
+			<span v-else>-</span>
+		</template>
+		<template v-else>
+			{{ value }}
+		</template>
+	</div>
+</template>
+
+<script>
+import { server } from '@/data/env'
+
+export default {
+	props: {
+		value: {
+			type: [String, Number],
+			default: ''
+		},
+		type: {
+			type: String,
+			required: true,
+			validator: (val) => {
+				return ~['text', 'img'].indexOf(val)
+			}
+		},
+		attr: {
+			type: String,
+			required: true
+		}
+	},
+	data() {
+		return {
+			server
+		}
+	}
+}
+</script>
--- src/pages/settings/Global.vue
+++ src/pages/settings/Global.vue
@@ -1,8 +1,33 @@
 <template>
-	<div>Global</div>
+	<div class="global-settings">
+		<div class="title">{{ title }}</div>
+		<el-timeline>
+			<el-timeline-item
+				v-for="(row, index) in rows"
+				:key="index"
+				:timestamp="row.title"
+				placement="top"
+			>
+				<el-card shadow="none">
+					<frontend-item
+						:value="settings[row.attr]"
+						:type="row.type"
+						:attr="row.attr"
+					/>
+				</el-card>
+			</el-timeline-item>
+		</el-timeline>
+		<div class="btn-block">
+<!--			TODO-->
+			<el-button type="primary">
+				Редагувати
+			</el-button>
+		</div>
+	</div>
 </template>
 
 <script>
+import FrontendItem from '@/components/settings/FrontendItem'
 import breadcrumbs from '@/mixins/breadcrumbs'
 import sections from '@/data/sections'
 import menu from '@/data/menu'
@@ -13,8 +38,66 @@ export default {
 		{ title: menu[sections.settings].title, routeName: sections.settings },
 		{ title: menu[sections.settings].children[sections.settingsGlobal].title }
 	],
+	components: {
+		FrontendItem
+	},
 	mixins: [
 		breadcrumbs
-	]
+	],
+	data() {
+		return {
+			rows: [
+				{ title: 'Назва', attr: 'app_name', type: 'text' },
+				{ title: 'Favicon', attr: 'favicon', type: 'img' },
+				{ title: 'Назва вкладки', attr: 'meta_title', type: 'text' },
+				{ title: 'Фотографія при авторізації', attr: 'logo_auth', type: 'img' },
+				{ title: 'Фотографія в шапці', attr: 'logo_header', type: 'img' },
+			]
+		}
+	},
+	computed: {
+		settings() {
+			return this.$store.state.template.settings
+		},
+		title() {
+			return menu[sections.settings].children[sections.settingsGlobal].title
+		}
+	}
 }
 </script>
+
+<style lang="scss" scoped>
+.global-settings {
+	max-width: 600px;
+	margin: 20px auto;
+}
+
+.title {
+	text-align: center;
+	font-size: 1.4rem;
+	font-weight: bold;
+	margin-bottom: 30px;
+}
+
+/deep/ .el-timeline-item__timestamp {
+	font-size: .9rem;
+}
+
+/deep/ .el-card__body {
+	background: #fafafa;
+	line-height: 1.5;
+	img {
+		max-width: 100px;
+		max-height: 100px;
+	}
+}
+
+.btn-block {
+	margin-top: 15px;
+	text-align: center;
+	button {
+		max-width: 300px;
+		width: 100%;
+	}
+}
+</style>
