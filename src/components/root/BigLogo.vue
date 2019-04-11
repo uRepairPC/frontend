3de3fc2da396f52360d59a87fb1d16e2f0f2327f
Alexey Khruhsch <alexeykhr@outlook.com>
2019-04-11 22:58:09 +0300

    Update Auth page and set logo

---
+++ src/components/root/BigLogo.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="logo">
+		<img
+			v-if="settings.logo_auth"
+			:src="logo"
+			:alt="settings.app_name || 'logo'"
+		>
+		<span
+			v-else-if="settings.app_name"
+			class="title"
+		>
+			{{ settings.app_name }}
+		</span>
+		<logo-svg
+			v-else
+			class="custom"
+		/>
+	</div>
+</template>
+
+<script>
+import LogoSvg from '@/components/svg/Logo'
+import { server } from '@/data/env'
+
+export default {
+	components: {
+		LogoSvg
+	},
+	computed: {
+		settings() {
+			return this.$store.state.template.settings
+		},
+		logo() {
+			return server + this.settings.logo_auth
+		},
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+	text-align: center;
+	margin-bottom: 30px;
+	> img {
+		max-width: 250px;
+		max-height: 200px;
+	}
+	.title {
+		display: block;
+		font-weight: bold;
+		font-size: 1.5rem;
+		margin: 30px 0 70px;
+	}
+	.custom {
+		height: 170px;
+	}
+}
+</style>
---
+++ src/components/svg/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
+		<rect fill="#ffffff" x="0" y="0" width="300px" height="300px"
+		      class="logo-background-square"></rect>
+		<linearGradient gradientTransform="rotate(25)" id="26eac158-4071-f92a-e7f6-4e7b3cc2244b" x1="0%"
+		                y1="0%" x2="100%" y2="0%">
+			<stop offset="0%" style="stop-color: rgb(81, 164, 251); stop-opacity: 1;"></stop>
+			<stop offset="100%" style="stop-color: rgb(81, 164, 251); stop-opacity: 1;"></stop>
+		</linearGradient>
+		<g id="4ff6bd92-5c96-4d90-67d3-1a8b536edce9" fill="url(#26eac158-4071-f92a-e7f6-4e7b3cc2244b)"
+		   stroke="none" transform="matrix(0.6999999999999997,0,0,0.6999999999999997,45.00000000000004,44.999991321563755)">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+				<path
+					d="M17.572 23.933l4.106 7.095L32 13.189h-8.981V7.582L11.51.972 0 7.582v13.222l11.51 6.611 6.062-3.482zm10.96-8.744l-6.855 11.846-6.855-11.846h13.71z"></path>
+			</svg>
+		</g>
+	</svg>
+</template>
