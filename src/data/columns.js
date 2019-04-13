97e2acf418a982b1f7600fa9b99438f890cb57b3
Alexey Khruhsch <alexeykhr@outlook.com>
2019-04-13 09:50:04 +0300

    Move users columns to store

--- src/data/columns.js
+++ src/data/columns.js
@@ -1,41 +1,14 @@
 'use strict'
 
-import * as permissions from '@/enum/permissions'
 import StorageData from '@/classes/StorageData'
+
+// TODO Move all function/const to store
 
 /**
  * @type {array}
  * @deprecated
  */
 export const COLUMNS_DATES = ['updated_at', 'created_at']
-
-/**
- * Get from store.
- * - Support permissions -
- * @return {array}
- */
-function userData() {
-	const defaultActive = ['first_name', 'last_name', 'email', 'phone']
-
-	const columns = [
-		{ prop: 'id', label: 'ID', 'min-width': 70, sortable: 'custom' },
-		{ prop: 'first_name', label: 'Ім\'я', 'min-width': 150, sortable: 'custom' },
-		{ prop: 'middle_name', label: 'По-батькові', 'min-width': 150, sortable: 'custom' },
-		{ prop: 'last_name', label: 'Прізвище', 'min-width': 150, sortable: 'custom' },
-		{ prop: 'roles', label: 'Ролі', 'min-width': 150, permissions: permissions.ROLES_VIEW, disableSearch: true },
-		{ prop: 'email', label: 'E-mail', 'min-width': 250, sortable: 'custom' },
-		{ prop: 'phone', label: 'Телефон', 'min-width': 150, sortable: 'custom' },
-		{ prop: 'description', label: 'Опис', 'min-width': 250, disableSearch: true },
-		{ prop: 'updated_at', label: 'Оновлено', 'min-width': 150, sortable: 'custom' },
-		{ prop: 'created_at', label: 'Створений', 'min-width': 150, sortable: 'custom' }
-	]
-
-	const data = StorageData.columnUsers.length ? StorageData.columnUsers : defaultActive
-
-	return columns.map((column) => {
-		return { ...column, model: data.includes(column.prop) }
-	})
-}
 
 /**
  * Get from store.
@@ -89,9 +62,6 @@ function roleData() {
 /*
  * Export const
  */
-
-/** @return {array} */
-export const users = userData()
 
 /** @return {array} */
 export const equipments = equipmentData()
--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -1,7 +1,8 @@
 'use strict'
 
-import { users as userColumns } from '@/data/columns'
 import { includePermission } from '@/scripts/utils'
+import * as permissions from '@/enum/permissions'
+import StorageData from '@/classes/StorageData'
 import User from '@/classes/User'
 import Vue from 'vue'
 
@@ -51,7 +52,28 @@ const getters = {
 	 * Display on table.
 	 */
 	columns() {
-		return userColumns.filter(column => includePermission(column.permissions))
+		const defaultActive = ['first_name', 'last_name', 'email', 'phone']
+
+		const columns = [
+			{ prop: 'id', label: 'ID', 'min-width': 70, sortable: 'custom' },
+			{ prop: 'first_name', label: 'Ім\'я', 'min-width': 150, sortable: 'custom' },
+			{ prop: 'middle_name', label: 'По-батькові', 'min-width': 150, sortable: 'custom' },
+			{ prop: 'last_name', label: 'Прізвище', 'min-width': 150, sortable: 'custom' },
+			{ prop: 'roles', label: 'Ролі', 'min-width': 150, permissions: permissions.ROLES_VIEW, disableSearch: true },
+			{ prop: 'email', label: 'E-mail', 'min-width': 250, sortable: 'custom' },
+			{ prop: 'phone', label: 'Телефон', 'min-width': 150, sortable: 'custom' },
+			{ prop: 'description', label: 'Опис', 'min-width': 250, disableSearch: true },
+			{ prop: 'updated_at', label: 'Оновлено', 'min-width': 150, sortable: 'custom' },
+			{ prop: 'created_at', label: 'Створений', 'min-width': 150, sortable: 'custom' }
+		]
+
+		const data = StorageData.columnUsers.length ? StorageData.columnUsers : defaultActive
+
+		return columns
+			.filter(column => includePermission(column.permissions))
+			.map((column) => {
+				return { ...column, model: data.includes(column.prop) }
+			})
 	}
 }
 
