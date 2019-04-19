bfb884564e5ea25c3f9909bd9d3890488d7bc00b
Alexey Khruhsch <alexeykhr@outlook.com>
2019-04-19 15:40:55 +0300

    Try auto get translate

--- src/locale/languages/index.js
+++ src/locale/languages/index.js
@@ -7,29 +7,15 @@
 const languages = []
 
 // eslint-disable-next-line
-const files = require.context('.', true, /\.js$/)
+const files = require.context('.', false, /\.js$/)
 
 files.keys().forEach(key => {
 	if (key === './index.js') {
 		return
 	}
 
-	// Ex. ['en-US', 'folder', 'fileName]
-	const split = key.substring(2)
-		.replace(/.js/g, '')
-		.split('/')
-
-	const len = split.length
-	let current = languages
-
-	// We pass an array and collect an object from it
-	for (let i = 0; i < len - 1; i++) {
-		if (typeof current[split[i]] === 'undefined') {
-			current[split[i]] = {}
-		}
-		current = current[split[i]]
-	}
-	current[split[len - 1]] = files(key).default
+	const lang = key.substring(2, key.length - 3)
+	languages[lang] = files(key).default
 })
 
 export default languages
