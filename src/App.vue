34d4f0250c2e5285827b9ee4b09385cf169f86bd
Alexey Khruhsch <alexeykhr@outlook.com>
2019-04-23 13:34:06 +0300

    Revert to init i18n branch

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
 	<div id="app">
-		<!--TODO Remove-->
-		{{ $t('efwg.wergreh.erhre.global.name') }}
-		{{ $tc('efwg.wergreh.erhre.global.name1') }}
-		{{ $tc('efwg.wergreh.erhre.global.name2') }}
-		{{ $tc('efwg.wergreh.erhre.global.name3') }}
-		{{ $tc('efwg.w.erhre.global.name3') }}
 		<router-view />
 		<store-dialogs />
 	</div>
@@ -17,13 +11,6 @@ import StoreDialogs from '@/components/root/StoreDialogs'
 export default {
 	components: {
 		StoreDialogs
-	},
-	// TODO Remove
-	mounted() {
-		console.log(this.$t('global.name'))
-		console.log(this.$t('global.name'))
-		console.log(this.$t('global.name'))
-		console.log(this.$t('global.name2'))
 	}
 }
 </script>
