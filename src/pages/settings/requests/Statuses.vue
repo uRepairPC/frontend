f306dcd40f7fdf2b114984eaad1704a1c5be83b7
Alexey Khruhsch <alexeykhr@outlook.com>
2019-04-23 09:10:19 +0300

    Add requests (page, store, class)

---
+++ src/pages/settings/requests/Statuses.vue
@@ -0,0 +1,63 @@
+<template>
+	<basic-table
+		:list="list"
+		:loading="loading"
+		:columns="columns"
+		:dialogs="dialogs"
+		:permission-create="permissions.REQUESTS_CONFIG_CREATE"
+		:permission-edit="permissions.REQUESTS_CONFIG_EDIT"
+		:permission-delete="permissions.REQUESTS_CONFIG_DELETE"
+		@update="fetchRequest"
+	/>
+</template>
+
+<script>
+import { requestStatuses as columns } from '@/data/columns'
+import BasicTable from '@/components/settings/BasicTable'
+import * as permissions from '@/enum/permissions'
+import breadcrumbs from '@/mixins/breadcrumbs'
+import sections from '@/data/sections'
+import { mapState } from 'vuex'
+import menu from '@/data/menu'
+
+export default {
+	name: 'Models',
+	breadcrumbs: [
+		{ title: menu[sections.settings].title, routeName: sections.settings },
+		{ title: menu[sections.settings].children[sections.equipmentsModels].title }
+	],
+	components: {
+		BasicTable
+	},
+	mixins: [
+		breadcrumbs
+	],
+	data() {
+		return {
+			columns,
+			permissions,
+			dialogs: {
+				// create: CreateDialog,
+				// edit: EditDialog,
+				// delete: DeleteDialog
+			}
+		}
+	},
+	computed: {
+		...mapState({
+			loading: state => state.requestStatuses.loading,
+			list: state => state.requestStatuses.list
+		})
+	},
+	mounted() {
+		if (!this.list.length) {
+			this.fetchRequest()
+		}
+	},
+	methods: {
+		fetchRequest() {
+			this.$store.dispatch('requestStatuses/fetchList')
+		}
+	}
+}
+</script>
