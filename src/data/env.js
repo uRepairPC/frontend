c05b7e7252b64a676149abdff9f1b86271a59bd2
Alexey Khrushch <alexeykhr@outlook.com>
2019-04-27 09:09:49 +0300

    Remove last slash on proxyTarget

--- src/data/env.js
+++ src/data/env.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
 'use strict'
 
+/**
+ * Remove last slash if exists
+ * @param input
+ * @return {string}
+ */
+const withoutLastSlash = (input) => {
+	if (!input) {
+		return ''
+	}
+
+	if (input.slice(-1) === '/') {
+		return input.slice(0, input.length - 1)
+	}
+
+	return input
+}
+
 /** @return {boolean} */
 export const isDev = ['dev', 'development'].includes(process.env.NODE_ENV)
 
 /** @return {string} */
-export const proxyTarget = process.env.PROXY_TARGET || 'http://localhost'
+export const proxyTarget = withoutLastSlash(process.env.PROXY_TARGET) || 'http://localhost'
 
 /** @return {string} */
 export const server = isDev ? proxyTarget : location.origin
