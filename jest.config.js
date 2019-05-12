4b1893420f6a7254b6cb69c417ed22db2e061fbb
Alexey Khruhsch <alexeykhr@outlook.com>
2019-05-12 22:09:13 +0300

    fix: run tests

--- jest.config.js
+++ jest.config.js
@@ -18,15 +18,7 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
-  snapshotSerializers: [
-    'jest-serializer-vue'
-  ],
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-  ],
-  testURL: 'http://localhost/',
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname'
   ]
 }
